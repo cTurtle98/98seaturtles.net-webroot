fa6304a452bb845c52ce460ccf6415378e98be11
Ciaran Farley <ciaran@cturtle98.com>
2019-08-01 22:22:10 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>98SeaTurtles Network</title>
+        <meta name="description" content="The Projects of Ciaran Farley">
+        <link rel="stylesheet" href="/css/main.css">
+		<?php include $_SERVER['DOCUMENT_ROOT'] . "/include/head.php"; ?>
+    </head>
+    <body>
+        <?php
+			include $_SERVER['DOCUMENT_ROOT'] . "/include/header.html";
+            include $_SERVER['DOCUMENT_ROOT'] . "/include/menu.html";
+            include $_SERVER['DOCUMENT_ROOT'] . "/include/ad/right-collum.php";
+        ?>
+        <div id="content">
+			<div id="content-title">
+				Home
+			</div>
+			<div id="content-body">
+            	Welcome to the 98SeaTurtles Website
+            	<br>
+            	This website is a collection of Ciarán Farley's projects and hobbies
+                <br><br>
+                Check out my <a href="http://ciaranfarley.info" target="_blank">Resumé on LinkedIn</a>
+				<br><br>
+				This website is and always will be a work in progress. Come back again and see what has changed.
+                <br><br>
+                <div id="content-subtitle">Featured Pages</div>
+                <ul>
+                    <li><a href="/ham/">Ham</a></li>
+                    <li><a href="social/ciaran/">Ciaran's Social Accounts</a></li>
+                </ul>
+                <br>
+                <div id="content-subtitle">Credits</div>
+                Special Thanks to <b>Satyam Love</b> of <a href="http://oneofthesevenbillion.com" target="_blank">OneOfTheSevenbillion</a> for helping me with adding php functionality to the site and for helping me with the program to make the meme and photography pages work. <br>
+                <br>
+                Special thanks to <b>Nick Stefanisko</b> of <a href="http://nick.stefanisko.net" target="_blank">nick.stefanisko.net</a> for helping me with attempting to make the menu fancy (I failed not him) and giving me advice on file size for the photography page. And for hepling me with adding the last edited date to the footer.
+				<br><br><br><br>
+                <div id="content-subtitle">Powered By</div>
+                <a href="http://apache.org"><img src="include/Apache_PoweredBy.svg" height="100px"></a>
+                <a href="http://debian.org"><img src="/include/debianlogo.svg" height="100px"></a> <br>
+                <br>
+                 
+                <div class="subtitle" id="Badges">Event Badges</div>
+                <a href="//makerfaire.com"><img src="events/MF15BA_Badge-10Years.jpg" alt="Bay Area Maker Faire 2015 Web Badge" width="150" height="150" border="0" /></a>
+                <a href="//makerfaire.com"><img src="events/MF16BA_Badge_255.png" alt="Maker Faire Bay Area 2016" width="150" height="150" border="0" /></a>
+                <a href="//makerfaire.com"><img src="events/MF17BA_badge.png" alt="Maker Faire Bay Area 2017" width="150" height="150" border="0"></a>
+                <a href="//makerfaire.com/bay-area/"><img src="events/MF18BA_Badge.png" alt="Maker Faire Bay Area" width="150" height="150" border="0" /></a>
+
+			</div>
+            <div class="fb-comments" data-href="http://98seaturtles.net" data-numposts="5"></div>
+        </div>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/include/footer.html";?>
+    </body>
+</html>
